802798cd43ae29c7bc68be853299d7f50660f90e
OguraDaiki <8hachibee125@gmail.com>
2021-02-04 14:22:22 +0900

    add express integration

--- api/app.ts
+++ api/app.ts
@@ -1,7 +1,11 @@
 import 'nexus-typegen';
 
-import {server} from './server';
+import {server, apollo} from './server';
 
-server.listen().then(({url}) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+// server.listen().then(({url}) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
+
+server.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${apollo.graphqlPath}`)
+);
--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,13 @@
-import {ApolloServer} from 'apollo-server';
+import {ApolloServer} from 'apollo-server-express';
+import express from 'express';
 
 import {schema} from './schema';
 import {context} from './context';
 
-export const server = new ApolloServer({schema, context});
+
+
+export const apollo = new ApolloServer({schema, context});
+
+export const server = express();
+
+apollo.applyMiddleware({app: server});
